8901cb55b1ae0b88d119271f9a164c07ecf38e0b
윤혜림 <rimee.yun@pulmuone.com>
2023-08-05 23:30:09 +0900

    컴포넌트 파일 생성, 등록

---
+++ vue-todo/src/components/toDoInput.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <input type="text" v-model="newTodoItem">
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data: function() {
+      return {
+        newTodoItem: ""
+      }
+    }
+  }
+  </script>
