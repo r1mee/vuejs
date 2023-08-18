ecee5360adb1bcbc1b5f4510470a5e08a4b1f3dc
윤혜림 <rimee.yun@pulmuone.com>
2023-08-18 11:11:41 +0900

    router import 수정

--- vue-todo/src/router/index.js
+++ vue-todo/src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
 
 Vue.use(VueRouter)
 
@@ -7,12 +9,12 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: () => import(/* webpackChunkName: "home" */ '../views/HomeView.vue')
+    component: HomeView
   },
   {
     path: '/about',
     name: 'about',
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: AboutView
   },
 ]
 
